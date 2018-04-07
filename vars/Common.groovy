79c770140076791c4f64f0faf25d50deda2a8e27
Shawn Lassiter <Shawn.Lassiter@gmail.com>
2018-04-07 06:58:29 -0400

    oops

--- vars/Common.groovy
+++ vars/Common.groovy
@@ -16,58 +16,59 @@ def runGitMergeFromBranch(git_branch, git_base_branch, git_repo_url){
 }
 
 void slack(msg){
-echo msg
-slackSend botUser: true, message: "${JOB_NAME}#${BUILD_ID}: ${msg}", tokenCredentialId: 'slack'
+  echo msg
+  slackSend botUser: true, message: "${JOB_NAME}#${BUILD_ID}: ${msg}", tokenCredentialId: 'slack'
 
 }
 
 def buildContainer(containerName){
-withCredentials([usernamePassword(credentialsId: 'odos-password', passwordVariable: 'ODOS_PW', usernameVariable: 'ODOS_USER')]) {
-    sh """
-      docker login -u ${ODOS_USER} -p ${ODOS_PW} docker.lassiterdynamics.com:5000
-      ./gradlew buildDocker
-      docker tag docker.lassiterdynamics.com:5000/${containerName}:latest docker.lassiterdynamics.com:5000/${containerName}:${BUILD_ID}
-    """
-}
+  withCredentials([usernamePassword(credentialsId: 'odos-password', passwordVariable: 'ODOS_PW', usernameVariable: 'ODOS_USER')]) {
+      sh """
+        docker login -u ${ODOS_USER} -p ${ODOS_PW} docker.lassiterdynamics.com:5000
+        ./gradlew buildDocker
+        docker tag docker.lassiterdynamics.com:5000/${containerName}:latest docker.lassiterdynamics.com:5000/${containerName}:${BUILD_ID}
+      """
+  }
 }
 
 def twistlock(repo,image,tag){
-twistlockScan(
-  ca: '',
-  cert: '',
-  compliancePolicy: 'high',
-  dockerAddress: 'unix:///var/run/docker.sock',
-  gracePeriodDays: 0,
-  ignoreImageBuildTime: false,
-  repository: repo,
-  image: "${image}:${tag}",
-  tag: tag,
-  key: '',
-  logLevel: 'true',
-  policy: 'high',
-  requirePackageUpdate: true,
-  timeout: 10
-)
+  twistlockScan(
+    ca: '',
+    cert: '',
+    compliancePolicy: 'high',
+    dockerAddress: 'unix:///var/run/docker.sock',
+    gracePeriodDays: 0,
+    ignoreImageBuildTime: false,
+    repository: repo,
+    image: "${image}:${tag}",
+    tag: tag,
+    key: '',
+    logLevel: 'true',
+    policy: 'high',
+    requirePackageUpdate: true,
+    timeout: 10
+  )
 
-twistlockPublish(
-  ca: '',
-  cert: '',
-  dockerAddress: 'unix:///var/run/docker.sock',
-  image: "${repo}/${image}:${tag}",
-  key: '',
-  logLevel: 'true',
-  timeout: 10
-)
+  twistlockPublish(
+    ca: '',
+    cert: '',
+    dockerAddress: 'unix:///var/run/docker.sock',
+    image: "${repo}/${image}:${tag}",
+    key: '',
+    logLevel: 'true',
+    timeout: 10
+  )
 }
 
 def deployToOpenShift(environment,image,tag){
-withCredentials([string(credentialsId: 'odos-jenkins-token', variable: 'OCP_TOKEN')]) {
-  sh """
-  oc login https://api.pro-us-east-1.openshift.com --token=${OCP_TOKEN}
-  oc project ${environment}
+  withCredentials([string(credentialsId: 'odos-jenkins-token', variable: 'OCP_TOKEN')]) {
+    sh """
+    oc login https://api.pro-us-east-1.openshift.com --token=${OCP_TOKEN}
+    oc project ${environment}
 
-  oc import-image ${image} \
-    --from='docker.lassiterdynamics.com:5000/${image}:${tag}' \
-    --confirm
-  """
+    oc import-image ${image} \
+      --from='docker.lassiterdynamics.com:5000/${image}:${tag}' \
+      --confirm
+    """
+  }
 }
